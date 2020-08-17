67810b304cb2db318a4d8a085e2293a45b58e843
akash-ak.kumar@broadcom.com <ak673050@broadcom.net>
2020-08-17 16:40:39 +0530

    added basic template in C++ for contest purpose

---
+++ basicTemplate.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+
+int main() {
+	// your code goes here
+	ll t; 
+	cin>>t;
+	while(t--){
+	    ll n,x;
+	    cin>>n>>k;
+	    vector<ll> a; 
+	    for(int i=0;i<n;i++){
+	        cin>>x;
+	        a.pb(x);
+	    }
+	    
+	}
+	
+	return 0;
+}
